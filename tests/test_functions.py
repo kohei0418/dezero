3fca70842b6365c0ef587a976b6cfd51b4e526c9
kohei <light.amp.ser+git@gmail.com>
2021-09-24 00:15:16 +0900

    draw graph

--- tests/test_functions.py
+++ tests/test_functions.py
@@ -4,6 +4,7 @@ import numpy as np
 
 from dezero import add, mul, square, train, Square, Variable
 from dezero.diff import numerical_diff
+from dezero.functions import matyas, goldstein
 
 
 class TestAdd(unittest.TestCase):
@@ -108,10 +109,6 @@ class TestTopologicalOrder(unittest.TestCase):
 class TestComplicatedFunctions(unittest.TestCase):
 
     def test_matyas(self):
-        def matyas(x, y):
-            z = 0.26 * (x ** 2 + y ** 2) - 0.48 * x * y
-            return z
-
         x = Variable(np.array(1.0))
         y = Variable(np.array(1.0))
         with train():
@@ -121,11 +118,6 @@ class TestComplicatedFunctions(unittest.TestCase):
         self.assertAlmostEqual(np.array(0.04), y.grad)
 
     def test_goldstein_price(self):
-        def goldstein(x, y):
-            z = (1 + (x + y + 1) ** 2 * (19 - 14 * x + 3 * x ** 2 - 14 * y + 6 * x * y + 3 * y ** 2)) * \
-                (30 + (2 * x - 3 * y) ** 2 * (18 - 32 * x + 12 * x ** 2 + 48 * y - 36 * x * y + 27 * y ** 2))
-            return z
-
         x = Variable(np.array(1.0))
         y = Variable(np.array(1.0))
         with train():
