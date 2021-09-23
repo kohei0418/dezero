9397d1cd496f9eb4d6e54338fbe1ad6f33c3b9c1
kohei <light.amp.ser+git@gmail.com>
2021-09-23 23:48:12 +0900

    prove auto diff is correct

--- tests/test_functions.py
+++ tests/test_functions.py
@@ -103,3 +103,33 @@ class TestTopologicalOrder(unittest.TestCase):
         self.assertIsNone(t.grad)
         self.assertEqual(np.array(2.0), x0.grad)
         self.assertEqual(np.array(1.0), x1.grad)
+
+
+class TestComplicatedFunctions(unittest.TestCase):
+
+    def test_matyas(self):
+        def matyas(x, y):
+            z = 0.26 * (x ** 2 + y ** 2) - 0.48 * x * y
+            return z
+
+        x = Variable(np.array(1.0))
+        y = Variable(np.array(1.0))
+        with train():
+            z = matyas(x, y)
+            z.backward()
+        self.assertAlmostEqual(np.array(0.04), x.grad)
+        self.assertAlmostEqual(np.array(0.04), y.grad)
+
+    def test_goldstein_price(self):
+        def goldstein(x, y):
+            z = (1 + (x + y + 1) ** 2 * (19 - 14 * x + 3 * x ** 2 - 14 * y + 6 * x * y + 3 * y ** 2)) * \
+                (30 + (2 * x - 3 * y) ** 2 * (18 - 32 * x + 12 * x ** 2 + 48 * y - 36 * x * y + 27 * y ** 2))
+            return z
+
+        x = Variable(np.array(1.0))
+        y = Variable(np.array(1.0))
+        with train():
+            z = goldstein(x, y)
+            z.backward()
+        self.assertAlmostEqual(np.array(-5376.0), x.grad)
+        self.assertAlmostEqual(np.array(8064.0), y.grad)
